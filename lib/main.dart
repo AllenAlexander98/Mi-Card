c91cb96ea4d2849020cee4080b8890ebf4188621
AllenAlexander98 <73653888+AllenAlexander98@users.noreply.github.com>
2021-09-08 14:33:06 +0700

    My First Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.pinkAccent.shade100,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('images/allen.jpg'),
+              ),
+              Text(
+                'Allen Tran',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30,
+                  color: Colors.deepPurpleAccent,
+                ),
+              ),
+              Text(
+                'Full Stack Developer',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 30,
+                  color: Colors.deepPurple,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.deepPurple,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  horizontalTitleGap: 5,
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.orange[900],
+                  ),
+                  title: Text(
+                    '+84 918 310 158',
+                    style: TextStyle(
+                        color: Colors.orange[900],
+                        fontSize: 20,
+                        fontFamily: 'SourceSansPro'),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                  horizontalTitleGap: 5,
+                  leading: Icon(
+                    Icons.mail_outline,
+                    color: Colors.orange[900],
+                    size: 25,
+                  ),
+                  title: Text(
+                    'zzallentranzz@gmail.com',
+                    style: TextStyle(
+                        color: Colors.orange[900],
+                        fontSize: 20,
+                        fontFamily: 'SourceSansPro'),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
